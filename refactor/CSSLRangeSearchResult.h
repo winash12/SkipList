eb8cd5e03a82f4b86ef51e5f65c19c09621952fe
Ashwin <winash12@gmail.com>
2020-01-24 08:30:28 +0530

    added csslproxynode.h

--- refactor/CSSLRangeSearchResult.h
+++ refactor/CSSLRangeSearchResult.h
@@ -5,7 +5,7 @@ class CSSLRangeSearchResult
  private:
   CSSLDataNode startNode;
   CSSLDataNode endNode;
-  int count;
+  int count =0;
  public:
   CSSLDataNode getStart();
   CSSLDataNode getEnd();
